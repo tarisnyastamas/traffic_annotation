7a77757bde6dc7be5ea8fa7dd9b33d78c03b56c2
tarisnyastamas <75031662+tarisnyastamas@users.noreply.github.com>
2021-06-09 21:28:39 +0300

    Add files via upload

---
+++ First_map/gettextvalue.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def my_form():
+    return render_template('map.html')
+
+@app.route('/', methods=['POST'])
+
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    print(processed_text)
+    return str(processed_text)
+    
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
